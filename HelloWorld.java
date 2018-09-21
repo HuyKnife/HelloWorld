1fdfab215af59574a60866e149768e7de7133adc
Đào Anh Huy <43365727+HuyKnife@users.noreply.github.com>
2018-09-21 23:10:43 +0700

    Accept input from user and display "Hello World I am XXX" where XXX is the user

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;;
 public class HelloWorld{
     public static void main(String[] args) {
-      System.out.println("Hello World I am Huy");
+      String name ="";
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Input your name: ");
+      name = sc.next();
+      System.out.println("Hello World I am " + name);
    }
 }
