3c97a2cac10c40845cf6c7865af718545f7399b6
Đào Anh Huy <43365727+HuyKnife@users.noreply.github.com>
2018-09-24 13:47:28 +0700

    add a count variables inside while loop to count number of student

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class HelloWorld{
     public static void main(String[] args) throws FileNotFoundException {
+        int count = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Input student list: ");
         File file = new File(sc.nextLine());
@@ -11,6 +12,7 @@ public class HelloWorld{
         System.out.println("Hello World This is TDTU");
         while(sc.hasNextLine()){
             System.out.print("Hello World I am " + sc.nextLine()+ "\n");
+            count++;
         }
    }
 }
